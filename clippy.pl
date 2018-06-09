23a8213bce357a66edebab143ce4b459e220f1fc
Oss was here <adamossenford@gmail.com>
2018-06-09 02:51:03 -0500

    removed commented out shit

--- clippy.pl
+++ clippy.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 use Text::Wrap;
 $Text::Wrap::columns = 40;
-#$string = join(" ", @ARGV);
 print "******************************************\n";
 print wrap('* ', '* ', @ARGV);
 print "\n******************************************\n";
